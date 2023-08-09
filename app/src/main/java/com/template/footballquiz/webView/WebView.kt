e6fa8fab48ee79324fe62ad65e353f8a680a7056
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-08-09 09:19:13 +0600

    ...

--- app/src/main/java/com/template/footballquiz/webView/WebView.kt
+++ app/src/main/java/com/template/footballquiz/webView/WebView.kt
@@ -15,34 +15,37 @@ import com.template.footballquiz.R
 
 @Suppress("DEPRECATION")
 class WebViewFragment : Fragment() {
-
     companion object {
-        private const val KEY_WEB_URL = "web_url"
-        private const val DEFAULT_WEB_URL = "https://example.com"
+        private const val KEY_WEB_URL = "url"
+        private const val DEFAULT_WEB_URL = "https://www.google.com"
     }
-
     private lateinit var webView: WebView
     private lateinit var firebaseRemoteConfig: FirebaseRemoteConfig
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_webview, container, false)
         webView = view.findViewById(R.id.webView)
         return view
     }
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
-        val configSettings = FirebaseRemoteConfigSettings.Builder()
-            .setMinimumFetchIntervalInSeconds(3600)
-            .build()
+        val configSettings =
+            FirebaseRemoteConfigSettings.Builder().setMinimumFetchIntervalInSeconds(3600).build()
         firebaseRemoteConfig.setConfigSettingsAsync(configSettings)
-        val webUrl = firebaseRemoteConfig.getString(KEY_WEB_URL, DEFAULT_WEB_URL)
+        firebaseRemoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val webUrl = firebaseRemoteConfig.getString(KEY_WEB_URL)
+                setupWebView(webUrl)
+            } else {
+                setupWebView(DEFAULT_WEB_URL)
+            }
+        }
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView(webUrl: String?) {
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true
@@ -55,8 +58,11 @@ class WebViewFragment : Fragment() {
         webView.webViewClient = WebViewClient()
         val cookieManager = CookieManager.getInstance()
         cookieManager.setAcceptCookie(true)
-        webView.loadUrl(webUrl)
+        if (webUrl != null) {
+            webView.loadUrl(webUrl)
+        }
     }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         webView.saveState(outState)
